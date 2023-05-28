13f0ee23707dadbb36f603fafc0b74285a12574a
Roma Nouke Frets Mantiri <romamantiri17@gmail.com>
2023-05-28 16:58:20 +0800

    "add Tampilan awal and scan qr 1"

---
+++ app/src/main/java/com/example/BRoCAF/TampilanAwal_activity.java
@@ -0,0 +1,14 @@
+package com.example.BRoCAF;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TampilanAwal_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampilan_awal);
+    }
+}
---
+++ app/src/main/java/com/example/BRoCAF/qrcode1_activity.java
@@ -0,0 +1,14 @@
+package com.example.BRoCAF;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class qrcode1_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode1);
+    }
+}
