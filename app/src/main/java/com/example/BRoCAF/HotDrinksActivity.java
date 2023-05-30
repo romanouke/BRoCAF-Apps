1ed435112eec8428148bff72f2f85e91527f8e6b
Giordyano <giordyanoanhar106@student.unsrat.ac.id>
2023-05-30 11:49:27 +0800

    add 4 tampilan menu

---
+++ app/src/main/java/com/example/BRoCAF/HotDrinksActivity.java
@@ -0,0 +1,14 @@
+package com.example.BRoCAF;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HotDrinksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hot_drinks);
+    }
+}
---
+++ app/src/main/java/com/example/BRoCAF/SquashAndFloatActivity.java
@@ -0,0 +1,14 @@
+package com.example.BRoCAF;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SquashAndFloatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_squash_and_float);
+    }
+}
