2b643ebd332fd00637cfb93aac464bdc77c9c417
Roma Nouke Frets Mantiri <romamantiri17@gmail.com>
2023-06-02 15:55:21 +0800

    add all menu

---
+++ app/src/main/java/com/example/BRoCAF/menu_nasi_ayam_activity.java
@@ -0,0 +1,14 @@
+package com.example.BRoCAF;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class menu_nasi_ayam_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_nasi_ayam);
+    }
+}
