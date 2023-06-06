cecd223ed3a4e2393b73f8cd600c68e436a59fe8
Roma Nouke Frets Mantiri <romamantiri17@gmail.com>
2023-06-07 01:04:45 +0800

    add karyawan and owner page

---
+++ app/src/main/java/com/example/BRoCAF/tampilan_utama_kp_activity.java
@@ -0,0 +1,14 @@
+package com.example.BRoCAF;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tampilan_utama_kp_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampilan_utama_kp);
+    }
+}
