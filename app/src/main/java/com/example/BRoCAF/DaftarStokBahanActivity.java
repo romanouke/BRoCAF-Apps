f374525102029e9f439472ecd2ed8657ccc831bd
albanijohanes <101566380+albanijohanes@users.noreply.github.com>
2023-06-08 22:41:38 +0700

    Modified daftarstock and fix qrcode(#1) (#2)
    * add java qr
    
    * fix daftarstock and change qr code

--- app/src/main/java/com/example/BRoCAF/DaftarStokBahanActivity.java
+++ app/src/main/java/com/example/BRoCAF/DaftarStokBahanActivity.java
@@ -1,56 +1,86 @@
 package com.example.BRoCAF;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
-public class DaftarStokBahanActivity extends AppCompatActivity implements View.OnClickListener {
+import androidx.appcompat.app.AppCompatActivity;
 
-    private ImageButton addButton1;
-    private ImageButton addButton2;
-    private ImageButton addButton3;
-    private ImageButton addButton4;
-    private ImageButton addButton5;
-    private ImageButton addButton6;
-    private ImageButton addButton7;
-    private ImageButton addButton8;
+public class DaftarStokBahanActivity extends AppCompatActivity {
+
+    private ImageButton[] addButton;
+    private  ImageButton[] subtractButton;
+    private TextView[] TextView;
+    private int[] itemValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar_stok_bahan);
 
-        addButton1 = findViewById(R.id.addButton1);
-        addButton2 = findViewById(R.id.addButton2);
-        addButton3 = findViewById(R.id.addButton3);
-        addButton4 = findViewById(R.id.addButton4);
-        addButton5 = findViewById(R.id.addButton5);
-        addButton6 = findViewById(R.id.addButton6);
-        addButton7 = findViewById(R.id.addButton7);
-        addButton8 = findViewById(R.id.addButton8);
+        TextView = new TextView[8];
+        addButton = new ImageButton[8];
+        subtractButton = new ImageButton[8];
+        itemValues = new int[8];
 
-        addButton1.setOnClickListener(this);
-        addButton2.setOnClickListener(this);
-        addButton3.setOnClickListener(this);
-        addButton4.setOnClickListener(this);
-        addButton5.setOnClickListener(this);
-        addButton6.setOnClickListener(this);
-        addButton7.setOnClickListener(this);
-        addButton8.setOnClickListener(this);
+        TextView[0] = findViewById(R.id.textView100);
+        TextView[1] = findViewById(R.id.textView101);
+        TextView[2] = findViewById(R.id.textView102);
+        TextView[3] = findViewById(R.id.textView103);
+        TextView[4] = findViewById(R.id.textView104);
+        TextView[5] = findViewById(R.id.textView107);
+        TextView[6] = findViewById(R.id.textView106);
+        TextView[7] = findViewById(R.id.textView105);
+
+        addButton[0] = findViewById(R.id.addButton1);
+        addButton[1] = findViewById(R.id.addButton2);
+        addButton[2] = findViewById(R.id.addButton3);
+        addButton[3] = findViewById(R.id.addButton4);
+        addButton[4] = findViewById(R.id.addButton5);
+        addButton[5] = findViewById(R.id.addButton6);
+        addButton[6] = findViewById(R.id.addButton7);
+        addButton[7] = findViewById(R.id.addButton8);
+
+        subtractButton[0] = findViewById(R.id.subtractButton1);
+        subtractButton[1] = findViewById(R.id.subtractButton2);
+        subtractButton[2] = findViewById(R.id.subtractButton3);
+        subtractButton[3] = findViewById(R.id.subtractButton4);
+        subtractButton[4] = findViewById(R.id.subtractButton5);
+        subtractButton[5] = findViewById(R.id.subtractButton6);
+        subtractButton[6] = findViewById(R.id.subtractButton7);
+        subtractButton[7] = findViewById(R.id.subtractButton8);
+
+        for(int i = 0; i < itemValues.length; i++) {
+            itemValues[i] = 0;
+            updateTextView(i);
+        }
+        for(int i = 0; i < addButton.length; i++){
+            final int index = i;
+            addButton[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    itemValues[index]++;
+                    updateTextView(index);
+                }
+            });
+        }
+        for(int i = 0; i < subtractButton.length; i++){
+            final int index = i;
+            subtractButton[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    itemValues[index]--;
+                    updateTextView(index);
+                }
+            });
+        }
+
+
     }
 
-    @Override
-    public void onClick(View view) {
-
+    private void updateTextView(int index) {
+        TextView[index].setText(String.valueOf(itemValues[index]));
     }
+
 }
