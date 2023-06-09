3c5fdb39b3e3bd12d2cb8868dc86cf96ae835988
albanijohanes <101566380+albanijohanes@users.noreply.github.com>
2023-06-09 14:53:22 +0700

    Create main java and edit tampilan (#3)
    * add java qr
    
    * fix daftarstock and change qr code
    
    * create main java and edit tampilan awal
    
    ---------
    
    Co-authored-by: albanijohanes <albanijohanes106@student.unsrat.ac.id>

--- app/src/main/java/com/example/BRoCAF/MainActivity.java
+++ app/src/main/java/com/example/BRoCAF/MainActivity.java
@@ -1,14 +1,74 @@
 package com.example.BRoCAF;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton ButtonBoba;
+    private ImageButton ButtonFood;
+    private ImageButton ButtonDrink;
+    private ImageButton ButtonHotdrink;
+    private ImageButton ButtonTambahHP;
+    private ImageButton ButtonKeranjang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButtonBoba = findViewById(R.id.Button_Boba);
+        ButtonFood = findViewById(R.id.Button_Food);
+        ButtonDrink = findViewById(R.id.Button_Drink);
+        ButtonHotdrink = findViewById(R.id.Button_Hotdrink);
+        ButtonTambahHP = findViewById(R.id.Button_tambahHP);
+        ButtonKeranjang = findViewById(R.id.Button_Keranjang);
+
+        ButtonBoba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BobaActivity.class);
+                startActivity(intent);
+            }
+        });
+        ButtonFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MakananActivity.class);
+                startActivity(intent);
+            }
+        });
+        ButtonDrink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SquashAndFloatActivity.class);
+                startActivity(intent);
+            }
+        });
+        ButtonHotdrink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HotDrinksActivity.class);
+                startActivity(intent);
+            }
+        });
+//        BELUM
+//        ButtonTambahHP.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, )
+//            }
+//        });
+        ButtonKeranjang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, menu_activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
